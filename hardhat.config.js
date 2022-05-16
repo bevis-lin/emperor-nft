45c87048b19907a0feada09dd584399ceb286508
Bevis Lin <bevis.lin@outlook.com>
2022-05-16 20:11:48 +0800

    change hardhat setting for local hardhat node

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,11 @@
-require('@nomiclabs/hardhat-waffle');
-require('dotenv').config();
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
 
 module.exports = {
-  solidity: '0.8.1',
+  defaultNetwork: "hardhat",
+  solidity: "0.8.1",
   networks: {
+    hardhat: {},
     matic: {
       url: `${process.env.ALCHEMY_MATIC_URL}`,
       accounts: [`0x${process.env.MATIC_PRIVATE_KEY}`],
